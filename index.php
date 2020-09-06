b1ca1b75a869ea6c4c1a12b567c26688ae25cc2f
s-bhageloe <2117940@talnet.nl>
2020-09-06 20:39:51 +0200

    Inlog pagina

---
+++ index.php
@@ -0,0 +1,13 @@
+<div class= "box"> 
+
+
+<form>
+ <input type="text" name="gebruikersnaam" placeholder=" "><br>
+ <input type="password" name="wachtwoord" placeholder=" "><br>
+<button type="submit" class="button">log in</button><br>
+
+<a class="vergeten" href=" ">Wachtwoord vergeten</a><br>
+<a class="vergeten" href=" ">Maak een nieuw account aan</a>
+</form>
+
+</div>
