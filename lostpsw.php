5981e4ae36b554c753fa4f0153c375de3a34d23e
s-bhageloe <2117940@talnet.nl>
2020-09-06 21:01:00 +0200

    Wachtwoord vergeten

---
+++ lostpsw.php
@@ -0,0 +1,10 @@
+<div class="vergeten-form">
+  <h1>Wachtwoord vergeten</h1>
+
+  <form>
+
+    <input type="text" name="Email" placeholder="Gebruikersnaam"/><br>
+    <button type="submit" class="button">Voltooi</button><br>
+
+  </form>
+</div>
