bc306501b395531feda115b114589474f1e9338f
s-bhageloe <2117940@talnet.nl>
2020-09-06 21:06:47 +0200

    Inlog pagina

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <h1>Inloggen</h1>
 
 <form>
-	
+
  <input type="text" name="gebruikersnaam" placeholder="Gebruikersnaam"><br>
  <input type="password" name="wachtwoord" placeholder="Wachtwoord"><br>
 <button type="submit" class="button">log in</button><br>
 
 <a class="vergeten" href="http://localhost/project1/signup.php">Maak een nieuw account aan</a><br>
-<a class="vergeten" href=" ">Wachtwoord vergeten</a>
+<a class="vergeten" href="http://localhost/project1/lostpsw.php">Wachtwoord vergeten</a>
 
 </form>
 </div>
