9966aa49741445428a38a81fa9c53e4f571559b8
s-bhageloe <2117940@talnet.nl>
2020-09-06 20:48:54 +0200

    Maak account aan pagina

---
+++ signup.php
@@ -0,0 +1,20 @@
+<div class= "box"> 
+
+<h1>Sign Up</h1>
+<form>
+
+ <input type="text" name="Voornaam" placeholder="Voornaam"><br>
+ <input type="text" name="Tussenvoegsel" placeholder="Tussenvoegsel"><br>
+ <input type="text" name="Achternaam" placeholder="Achternaam"><br>
+ <input type="text" name="E-mail" placeholder="E-mail"><br>
+ <input type="text" name="gebruikersnaam" placeholder="Gebruikersnaam"><br>
+ <input type="text" name="wachtwoord" placeholder="Wachtwoord"><br>
+ <input type="text" name="herhaal_wachtwoord" placeholder="Herhaal wachtwoord "><br>
+
+ <button type="submit" class="button">Maak account aan</button><br>
+
+<a class="vergeten" href=" ">Wachtwoord vergeten</a><br>
+<a class="vergeten" href=" ">Maak een nieuw account aan</a>
+</form>
+
+</div>
